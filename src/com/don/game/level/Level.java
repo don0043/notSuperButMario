0a9ed15c6628e379c480f1a87acf69ec386b5c1d
don0043 <daniiar.arkabaev@iaau.edu.kg>
2019-05-19 18:10:34 +0600

    updated level class

--- src/com/don/game/level/Level.java
+++ src/com/don/game/level/Level.java
@@ -9,11 +9,11 @@ import java.util.Map;
 
 public class Level {
 
-    public static final int TILE_SCALE = 8;
-    public static final int TILE_IN_GAME_SCALE = 1;
-    public static final int SCALED_TILE_SIZE = (TILE_SCALE * TILE_IN_GAME_SCALE);
-    public static final int TILES_IN_WIDTH = Game.WIDTH / SCALED_TILE_SIZE;
-    public static final int TILES_IN_HEIGHT = Game.HEIGHT / SCALED_TILE_SIZE;
+    private static final int TILE_SCALE = 8;
+    private static final int TILE_IN_GAME_SCALE = 2;
+    private static final int SCALED_TILE_SIZE = (TILE_SCALE * TILE_IN_GAME_SCALE);
+    private static final int TILES_IN_WIDTH = Game.WIDTH / SCALED_TILE_SIZE;
+    private static final int TILES_IN_HEIGHT = Game.HEIGHT / SCALED_TILE_SIZE;
 
 
 
@@ -35,6 +35,13 @@ public class Level {
         tileMap = new int[TILES_IN_WIDTH][TILES_IN_HEIGHT];
 
         tileMap[10][10] = TileType.BRICK.numeric();
+        tileMap[11][11] = TileType.BRICK.numeric();
+        tileMap[12][12] = TileType.BRICK.numeric();
+        tileMap[13][13] = TileType.BRICK.numeric();
+        tileMap[14][14] = TileType.BRICK.numeric();
+        tileMap[15][15] = TileType.BRICK.numeric();
+
+
 
 
     }
@@ -44,7 +51,7 @@ public class Level {
     public void render(Graphics2D g){
         for (int i=0; i<tileMap.length; i++){
             for(int j = 0; j < tileMap[i].length; j++){
-                tiles.get(TileType.fromNumeric(tileMap[j][i])).render(g, j * SCALED_TILE_SIZE, i * SCALED_TILE_SIZE);
+                tiles.get(TileType.fromNumeric(tileMap[i][j])).render(g, j * SCALED_TILE_SIZE, i * SCALED_TILE_SIZE);
             }
         }
 
