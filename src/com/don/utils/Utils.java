9078d4ba19c960b9ebb9cf504874691d148d0680
don0043 <daniiar.arkabaev@iaau.edu.kg>
2019-05-22 00:12:23 +0600

    Added 1st lvl

--- src/com/don/utils/Utils.java
+++ src/com/don/utils/Utils.java
@@ -1,7 +1,10 @@
 package com.don.utils;
 
 import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
     public static BufferedImage resize(BufferedImage image, int width, int height){
@@ -9,5 +12,41 @@ public class Utils {
        newImage.getGraphics().drawImage(image, 0, 0, width, height, null);
 
         return newImage;
+    }
+    public static Integer[][] levelParser(String filepath){
+
+        Integer[][] result = null;
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File(filepath)))){
+
+            String line = null;
+            List<Integer []> lvlLines;
+            for (Integer[] integers : lvlLines = new ArrayList<Integer[]>()) {
+                
+            }
+            ;
+            while ((line = reader.readLine())!= null) lvlLines.add(str2int_arrays(line.split(" ")));
+
+
+            result = new Integer[lvlLines.size()][lvlLines.get(0).length];
+            for(int i = 0; i < lvlLines.size(); i++){
+                result[i] = lvlLines.get(i);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return result;
+    }
+    public static final Integer[] str2int_arrays(String[] sArr){
+        Integer[] result = new Integer[sArr.length];
+        for(int i=0; i<sArr.length;i++) {
+            result[i] = Integer.parseInt(sArr[i]);
+        }
+        return result;
     }
 }
